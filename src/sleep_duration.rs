1350ff9988e6024af93005c71a605259f1581544
Joseph Lenton <josephlenton@gmail.com>
2024-01-26 13:02:57 +0000

    feat: add min max duration constructors to SleepDuration

--- src/sleep_duration.rs
+++ src/sleep_duration.rs
@@ -2,6 +2,7 @@ use rand::thread_rng;
 use rand::Rng;
 use std::ops::Range;
 use std::time::Duration;
+use tokio::time::sleep;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum SleepDuration {
@@ -10,8 +11,23 @@ pub enum SleepDuration {
 }
 
 impl SleepDuration {
+    pub const fn from_duration(duration: Duration) -> Self {
+        Self::Duration(duration)
+    }
+
+    pub const fn from_min_max_durations(min_duration: Duration, max_duration: Duration) -> Self {
+        Self::MinMax(min_duration, max_duration)
+    }
+
     pub const fn from_millis(millis: u64) -> Self {
         Self::Duration(Duration::from_millis(millis))
+    }
+
+    pub const fn from_min_max_millis(min_millis: u64, max_millis: u64) -> Self {
+        Self::MinMax(
+            Duration::from_millis(min_millis),
+            Duration::from_millis(max_millis),
+        )
     }
 
     pub fn into_duration(self) -> Duration {
@@ -19,6 +35,10 @@ impl SleepDuration {
             Self::Duration(duration) => duration,
             Self::MinMax(min, max) => thread_rng().gen_range(min..max),
         }
+    }
+
+    pub async fn sleep(self) {
+        sleep(self.into_duration()).await
     }
 }
 
